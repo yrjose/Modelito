258973adde1e0125ad17f76237bf7c701dda7dc6
yrjose <chemau2002@hotmail.com>
2025-09-05 17:28:20 -0300

    segundo teste

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ c = st.slider("Coeficiente c (x)", -10.0, 10.0, 0.0)
 d = st.slider("Coeficiente d (termo independente)", -10.0, 10.0, 0.0)
 
 #comentario massa 17:10 05/09/25
+#comentario muito massa 17:28 05/09/25
 # Intervalo de x
 x = np.linspace(-10, 10, 400)
 y = a*x**3 + b*x**2 + c*x + d
